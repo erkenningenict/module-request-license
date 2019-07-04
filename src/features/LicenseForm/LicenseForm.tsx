6e8ae91901e954a11d63c453ea507cc4e64add99
Gerwin Koudijs <gerwin.koudijs@gmail.com>
2019-07-04 15:50:45 +0200

    trigger

--- src/features/LicenseForm/LicenseForm.tsx
+++ src/features/LicenseForm/LicenseForm.tsx
@@ -152,7 +152,7 @@ class LicenseForm extends React.Component<{}, ILicenseFormState> {
           </Panel>
         ) : null}
         {this.state.currentStep === LicenseSteps.PersonalInfo ? (
-          <Panel title="Uw persoonsgegevens XXXX">
+          <Panel title="Uw persoonsgegevens XX2">
             <PersonalInfo {...props} />
           </Panel>
         ) : null}
