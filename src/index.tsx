2c0aa78f9770e18e8e80899f8575e7055c0cc9dc
Didier Hartong <info@focuswider.com>
2020-07-24 17:37:26 +0200

    feat(netlify): prepare for netlify

--- src/index.tsx
+++ src/index.tsx
@@ -34,5 +34,5 @@ ReactDOM.render(
       <App />
     </ThemeBureauErkenningen>
   </ApolloProvider>,
-  document.getElementById('root'),
+  document.getElementById('erkenningen-module-request-license'),
 );
