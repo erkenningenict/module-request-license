c284590503f1c2a9842fc4d27500dcfefda683e4
Didier Hartong <info@focuswider.com>
2024-01-19 15:32:00 +0100

    fix: cleanup unused code

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import env from 'vite-plugin-env-compatible';
 import dns from 'dns';
 
 // localhost part
@@ -16,18 +14,7 @@ export default defineConfig({
     host: 'localhost',
     // this sets a default port to 3000
     port: 3000,
-    strictPort: true
+    strictPort: true,
   },
-  define: {
-    // here is the main update
-    global: 'window',
-    'process.env': {
-      GATSBY_ERKENNINGEN_LOGIN_URL: '',
-      ERKENNINGEN_LOGIN_URL: '',
-      REACT_APP_ERKENNINGEN_LOGIN_URL: '',
-      ERKENNINGEN_ADMIN_URL: '',
-      ERKENNINGEN_SITE_TYPE: 'admin',
-    },
-  },
-  plugins: [react(), viteTsconfigPaths(), env()],
+  plugins: [react()],
 });
